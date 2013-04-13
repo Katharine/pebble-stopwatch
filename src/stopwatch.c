84090a7c1caff8c4e5ad366e1dc09235ff826e34
Katharine Berry <katharine@kathar.in>
2013-04-13 03:55:10 -0400

    Adjustments to display 100ths.
    Added a new font size, made seconds smaller, added an extra row of
    lap times to fix glitchiness as they fell off the end.

--- src/stopwatch.c
+++ src/stopwatch.c
@@ -20,8 +20,8 @@ Layer line_layer;
 
 
 // Lap time display
-#define LAP_TIME_SIZE 4
-char lap_times[LAP_TIME_SIZE][9] = {"00:00:00", "00:01:00", "00:02:00", "00:03:00"};
+#define LAP_TIME_SIZE 5
+char lap_times[LAP_TIME_SIZE][11] = {"00:00:00.0", "00:01:00.0", "00:02:00.0", "00:03:00.0", "00:04:00.0"};
 TextLayer lap_layers[LAP_TIME_SIZE]; // an extra temporary layer
 int next_lap_layer = 0;
 int last_lap_time = 0;
@@ -42,8 +42,9 @@ AppTimerHandle update_timer = APP_TIMER_INVALID_HANDLE;
 int busy_animating = 0;
 
 #define TIMER_UPDATE 1
-#define FONT_BIG_TIME RESOURCE_ID_FONT_DEJAVU_SANS_BOLD_SUBSET_32
-#define FONT_SECONDS RESOURCE_ID_FONT_DEJAVU_SANS_SUBSET_20
+#define FONT_BIG_TIME RESOURCE_ID_FONT_DEJAVU_SANS_BOLD_SUBSET_30
+#define FONT_SECONDS RESOURCE_ID_FONT_DEJAVU_SANS_SUBSET_18
+#define FONT_LAPS RESOURCE_ID_FONT_DEJAVU_SANS_SUBSET_22
 
 void toggle_stopwatch_handler(ClickRecognizerRef recognizer, Window *window);
 void config_provider(ClickConfig **config, Window *window);
@@ -78,13 +79,13 @@ void handle_init(AppContextRef ctx) {
     // Get our fonts
     GFont big_font = fonts_load_custom_font(resource_get_handle(FONT_BIG_TIME));
     GFont seconds_font = fonts_load_custom_font(resource_get_handle(FONT_SECONDS));
-    GFont laps_font = seconds_font;
+    GFont laps_font = fonts_load_custom_font(resource_get_handle(FONT_LAPS));
 
     // Root layer
     Layer *root_layer = window_get_root_layer(&window);
 
     // Set up the big timer.
-    text_layer_init(&big_time_layer, GRect(0, 5, 105, 35));
+    text_layer_init(&big_time_layer, GRect(0, 5, 96, 35));
     text_layer_set_background_color(&big_time_layer, GColorBlack);
     text_layer_set_font(&big_time_layer, big_font);
     text_layer_set_text_color(&big_time_layer, GColorWhite);
@@ -92,21 +93,21 @@ void handle_init(AppContextRef ctx) {
     text_layer_set_text_alignment(&big_time_layer, GTextAlignmentRight);
     layer_add_child(root_layer, &big_time_layer.layer);
 
-    text_layer_init(&seconds_time_layer, GRect(107, 17, 33, 35));
+    text_layer_init(&seconds_time_layer, GRect(96, 17, 49, 35));
     text_layer_set_background_color(&seconds_time_layer, GColorBlack);
     text_layer_set_font(&seconds_time_layer, seconds_font);
     text_layer_set_text_color(&seconds_time_layer, GColorWhite);
-    text_layer_set_text(&seconds_time_layer, ":00");
+    text_layer_set_text(&seconds_time_layer, ":00.0");
     layer_add_child(root_layer, &seconds_time_layer.layer);
 
     // Draw our nice line.
-    layer_init(&line_layer, GRect(0, 50, 144, 2));
+    layer_init(&line_layer, GRect(0, 45, 144, 2));
     line_layer.update_proc = &draw_line;
     layer_add_child(root_layer, &line_layer);
 
     // Set up the lap time layers. These will be made visible later.
     for(int i = 0; i < LAP_TIME_SIZE; ++i) {
-        text_layer_init(&lap_layers[i], GRect(-139, 57, 139, 30));
+        text_layer_init(&lap_layers[i], GRect(-139, 52, 139, 30));
         text_layer_set_background_color(&lap_layers[i], GColorClear);
         text_layer_set_font(&lap_layers[i], laps_font);
         text_layer_set_text_color(&lap_layers[i], GColorWhite);
@@ -170,6 +171,10 @@ void lap_time_handler(ClickRecognizerRef recognizer, Window *window) {
 }
 
 // There must be some way of doing this besides writing our own...
+void itoa1(int num, char* buffer) {
+    const char digits[10] = "0123456789";
+    buffer[0] = digits[num % 10];
+}
 void itoa2(int num, char* buffer) {
     const char digits[10] = "0123456789";
     if(num > 99) {
@@ -186,9 +191,10 @@ void itoa2(int num, char* buffer) {
 
 void update_stopwatch() {
     static char big_time[] = "00:00";
-    static char seconds_time[] = ":00";
+    static char seconds_time[] = ":00.0";
 
     // Now convert to hours/minutes/seconds.
+    int hundredths = (elapsed_time / 100) % 10;
     int seconds = (elapsed_time / 1000) % 60;
     int minutes = (elapsed_time / 60000) % 60;
     int hours = elapsed_time / 3600000;
@@ -202,6 +208,7 @@ void update_stopwatch() {
     itoa2(hours, &big_time[0]);
     itoa2(minutes, &big_time[3]);
     itoa2(seconds, &seconds_time[1]);
+    itoa1(hundredths, &seconds_time[4]);
 
     // Now draw the strings.
     text_layer_set_text(&big_time_layer, big_time);
@@ -240,6 +247,7 @@ void save_lap_time(int lap_time) {
 
     // Once those are done we can slide our new lap time in.
     // First we need to generate a string.
+    int hundredths = (lap_time / 100) % 10;
     int seconds = (lap_time / 1000) % 60;
     int minutes = (lap_time / 60000) % 60;
     int hours = lap_time / 3600000;
@@ -247,11 +255,12 @@ void save_lap_time(int lap_time) {
     itoa2(hours, &lap_times[next_lap_layer][0]);
     itoa2(minutes, &lap_times[next_lap_layer][3]);
     itoa2(seconds, &lap_times[next_lap_layer][6]);
+    itoa1(hundredths, &lap_times[next_lap_layer][9]);
 
     // Animate it
     static PropertyAnimation entry_animation;
-    static GRect origin; origin = GRect(-139, 57, 139, 26);
-    static GRect target; target = GRect(5, 57, 139, 26);
+    static GRect origin; origin = GRect(-139, 52, 139, 26);
+    static GRect target; target = GRect(5, 52, 139, 26);
     property_animation_init_layer_frame(&entry_animation, &lap_layers[next_lap_layer].layer, &origin, &target);
     animation_set_curve(&entry_animation.animation, AnimationCurveEaseOut);
     animation_set_delay(&entry_animation.animation, 50);
