db5f2839e14e7041ec87e0faa796eaa59f8da98a
Katharine Berry <katharine@kathar.in>
2013-04-13 04:12:46 -0400

    Added an icon and bumped version to 2.0.

--- src/stopwatch.c
+++ src/stopwatch.c
@@ -6,8 +6,8 @@
 #define MY_UUID { 0xC2, 0x5A, 0x8D, 0x50, 0x10, 0x5F, 0x45, 0xBF, 0xB9, 0x92, 0xCF, 0xF9, 0x58, 0xA7, 0x93, 0xAD }
 PBL_APP_INFO(MY_UUID,
              "Stopwatch", "Katharine Berry",
-             1, 0, /* App version */
-             DEFAULT_MENU_ICON,
+             2, 0, /* App version */
+             RESOURCE_ID_IMAGE_MENU_ICON,
              APP_INFO_STANDARD_APP);
 
 Window window;
