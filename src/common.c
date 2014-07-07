49daa5739f7986e6145cae63e39eea496b964e07
Katharine Berry <katharine@kathar.in>
2014-07-07 15:40:49 -0700

    Fiddle fiddle

--- src/common.c
+++ src/common.c
@@ -24,10 +24,10 @@
 #include <pebble.h>
 
 void format_lap(double lap_time, char* buffer) {
-    int tenths = (int)(lap_time * 10) % 10;
-    int seconds = (int)lap_time % 60;
-    int minutes = (int)lap_time / 60 % 60;
-    int hours = (int)lap_time / 3600;
+  int tenths = (int)(lap_time * 10) % 10;
+  int seconds = (int)lap_time % 60;
+  int minutes = (int)lap_time / 60 % 60;
+  int hours = (int)lap_time / 3600;
 
 	snprintf(buffer, 11, "%02d:%02d:%02d.%d", hours, minutes, seconds, tenths);
 }
